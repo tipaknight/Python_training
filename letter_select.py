114c9b574785dc663c17edce84781e8d13c6aa2b
tipaknight <tipa-knight@yandex.ru>
2022-01-28 10:51:41 +0500

    комент в letter_select

--- letter_select.py
+++ letter_select.py
@@ -1,7 +1,9 @@
+# создаем список слов
 words = ['python', 'c++', 'c', 'scala', 'java']
 
-
+# создаем функцию для выбора букв со списком слов и буквами в них
 def count_letter (words, letter):
+	# обозначаем выборку с первой буквы - это 0
 	count=0
 	for word in words:
 		if letter in word:
